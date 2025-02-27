a238f93c3325c0442999d842ba507fa55d70279a
Jônatas Bueno do Livramento <jonatas.livramento@gmail.com>
2025-02-27 16:42:40 -0300

    feat: first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
+
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, NgOptimizedImage],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
