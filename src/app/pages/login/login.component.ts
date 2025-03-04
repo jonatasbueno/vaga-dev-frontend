4ff723e3e059e1f359d3e27ca254abb4a01de930
Jônatas Bueno do Livramento <jonatas.livramento@gmail.com>
2025-03-04 19:58:21 -0300

    feat: Criando Footer

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  @Input() isLogged: boolean;
+
+  constructor() {
+    this.isLogged = false
+  }
+}
