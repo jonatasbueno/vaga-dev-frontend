698f4e6b324b64b8b4ffbfc72e63ae5c7de6016e
Jônatas Bueno do Livramento <jonatas.livramento@gmail.com>
2025-03-04 15:58:34 -0300

    feat: Criando estrutura inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
